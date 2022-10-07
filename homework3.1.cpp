18b36ef24d15e29cab5d1fe862bc3ef8bd5747c9
Arus2002 <115175915+Arus2002@users.noreply.github.com>
2022-10-07 19:02:44 +0400

    Add files via upload

---
+++ homework3.1.cpp
@@ -0,0 +1,33 @@
+#include "ComplexNum.h"
+#include <cmath>
+
+ComplexNum::ComplexNum() {};
+
+
+ComplexNum::ComplexNum(const double& re)
+	
+	:mRe{re}
+{}
+
+
+ComplexNum::ComplexNum(const double& re , const double& im )
+	:mRe{re}
+	,mIm{im}
+
+{}
+
+double ComplexNum::getModuls(){
+	return sqrt(mRe * mRe + mIm * mIm);
+}
+
+double ComplexNum::getRe()
+{
+	return mRe;
+}
+
+double ComplexNum::getIm()
+{
+        return mIm;
+}
+
+
